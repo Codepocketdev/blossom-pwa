91b24536be5154648935eae0f8f2499c0af55298
Codepocketdev <artsmartin44@gmail.com>
2026-01-13 17:36:17 +0300

    Clear all service worker caches

--- sw.js
+++ sw.js
@@ -1,38 +1,14 @@
-const CACHE_NAME = 'blossom-v2';
-const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
-  // Force the waiting service worker to become the active service worker
+// Unregister old service worker
+self.addEventListener('install', () => {
   self.skipWaiting();
 });
 
-self.addEventListener('activate', event => {
+self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then(cacheNames => {
+    caches.keys().then((cacheNames) => {
       return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
-        })
+        cacheNames.map((cacheName) => caches.delete(cacheName))
       );
-    })
-  );
-  // Take control of all pages immediately
-  return self.clients.claim();
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    }).then(() => self.clients.claim())
   );
 });
